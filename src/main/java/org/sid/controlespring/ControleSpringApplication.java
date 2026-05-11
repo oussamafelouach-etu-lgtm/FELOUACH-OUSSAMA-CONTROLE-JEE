596280a6e25715388c2ffa622860f81a3a8426fc
oussama <oussama.felouach-etu@etu.univh2c.ma>
2026-05-11 15:23:47 +0200

    first commit

---
+++ src/main/java/org/sid/controlespring/ControleSpringApplication.java
@@ -0,0 +1,13 @@
+package org.sid.controlespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/controlespring/ControleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.controlespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
